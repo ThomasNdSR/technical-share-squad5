cc85e9bb1f4e43be68a1ba41f9ab417c188a74cb
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-09 15:05:28 -0300

    feat: created header component

--- front-end/src/pages/Dashboard/index.jsx
+++ front-end/src/pages/Dashboard/index.jsx
@@ -1,8 +1,10 @@
 import { MentorCard } from "../../components/Dashboard/MentorCard";
+import { Header } from "../../components/Header";
 
 export const Dashboard = () => {
   return (
     <>
+      <Header />
       <MentorCard />
     </>
   );
