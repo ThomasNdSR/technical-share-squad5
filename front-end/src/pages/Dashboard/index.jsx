222a1f5fdfebbff4340c5074d5e4e5bddb6b10cb
André Martins <79060401+andresmartins910@users.noreply.github.com>
2022-04-09 12:22:20 -0300

    feat: mentor card structure and styles

--- front-end/src/pages/Dashboard/index.jsx
+++ front-end/src/pages/Dashboard/index.jsx
@@ -1,12 +1,9 @@
+import { MentorCard } from "../../components/Dashboard/MentorCard";
+
 export const Dashboard = () => {
   return (
     <>
-      <h1>Dashboard</h1>
-      <h2>Dashboard</h2>
-      <h3>Dashboard</h3>
-      <h4>Dashboard</h4>
-      <h5>Dashboard</h5>
-      <h6>Dashboard</h6>
+      <MentorCard />
     </>
   );
 };
