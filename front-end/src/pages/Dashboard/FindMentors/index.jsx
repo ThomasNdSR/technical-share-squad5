c06133699afdfca01060472fe9dc0e53d7ea6d12
Alan Lira <alan.lira08@gmail.com>
2022-04-11 02:46:20 -0300

    Feature: Adicionado a listagem de opções de sort dos cards

--- front-end/src/pages/Dashboard/FindMentors/index.jsx
+++ front-end/src/pages/Dashboard/FindMentors/index.jsx
@@ -1,3 +1,4 @@
+import { AiOutlineCalendar,AiOutlineContacts,AiOutlineSortAscending,AiOutlineSortDescending,MdEventAvailable } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import { BiSlider } from "react-icons/bi";
 import { MentorCard } from "../../../components/Dashboard/MentorCard";
@@ -6,11 +7,10 @@ import "./styles.css";
 
 export const FindMentors = () => {
   const [users, setUsers] = useState([]);
-
+  const [sliderActive, setSlider] = useState(false);
   useEffect(() => {
     getAllUsers();
   }, []);
-
   const getAllUsers = async () => {
     try {
       const response = await api.get("/user");
@@ -25,13 +25,24 @@ export const FindMentors = () => {
   return (
     <section className="discover-professionals-box">
       <div className="discover-container">
-      <h3 className="discover-professionals-heading">
-        Descubra profissionais na sua área
-      </h3>
-      <button className="slider">
-        <BiSlider size={39} color="var(--primary-03)" />
-      </button>
+        <h3 className="discover-professionals-heading">
+          Descubra profissionais na sua área
+        </h3>
+        <button
+          onClick={() => {
+            setSlider(!sliderActive);
+          }}
+          className="slider"
+        >
+          <BiSlider size={39} color="var(--primary-03)" />
+        </button>
       </div>
+      <ul className={`slider__sort ${sliderActive ? "active" : ""}`}>
+        <li><AiOutlineSortAscending size={16}color="var(--primary-03)"/> Ordem ascendente</li>
+        <li><AiOutlineSortDescending size={16}color="var(--primary-03)"/> Ordem descendente</li>
+        <li><AiOutlineCalendar size={16}color="var(--primary-03)"/> Disponibilidade</li>
+        <li><AiOutlineContacts size={16}color="var(--primary-03)"/> Da sua área</li>
+      </ul>
       <div className="discover-professionals-list">
         {users.map((user) => (
           <MentorCard
