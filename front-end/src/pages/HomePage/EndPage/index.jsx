f88dadbb42d6e05936008ef173036ede17ecb8f9
Alan Lira <alan.lira08@gmail.com>
2022-04-11 20:52:39 -0300

    Feature: Finalizando home page

--- front-end/src/pages/HomePage/EndPage/index.jsx
+++ front-end/src/pages/HomePage/EndPage/index.jsx
@@ -1,12 +1,18 @@
 import endPageImage from "../../../assets/endpage.png"
+import img1 from "../../../assets/01.svg";
+import img2 from "../../../assets/02.svg";
 import "./style.css";
+
 
 export const EndPage = () => {
   return (
     <div className="page">
+      
       <section className="endPage">
-      <h4>Encontre o #Sanguelaranja certo para te auxiliar com o seu problema</h4>
-      <img src={endPageImage} alt="Ilustração de colaboradores" />
+      <img src={img1} alt="decoração da pagina com pontos" className="endPage_decorative--spots" />
+      <h3>Encontre o #Sanguelaranja certo para te auxiliar com o seu problema</h3>
+      <img className="endPage__illustration" src={endPageImage} alt="Ilustração de colaboradores" />
+      <img src={img2}  alt="decoração da pagina com ondas" className="endPage_decorative--waves"/>
       </section>
     </div>
   );
