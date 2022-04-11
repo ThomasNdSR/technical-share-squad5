29d537caaed10d1761a9c62ee1f95fcd7d3130b8
Alan Lira <alan.lira08@gmail.com>
2022-04-10 21:13:47 -0300

    Feature: Colocando as opções de mais buscados

--- front-end/src/pages/Dashboard/SearchContainer/index.jsx
+++ front-end/src/pages/Dashboard/SearchContainer/index.jsx
@@ -4,23 +4,42 @@ import { RiSearch2Line } from "react-icons/ri";
 import "./styles.css";
 
 export const SearchContainer = () => {
+  const list = [
+    { skill: "Front-end" },
+    { skill: "Scrum" },
+    { skill: "FullStack" },
+    { skill: "Ux design" },
+  ];
   return (
-    <div className="search-component">
-      <img id="img1" src={img1} alt="" />
+    <section className="search-component">
+      <img id="img1" src={img1} alt="decoração da pagina com pontos" />
       <h2 id="find-mentors-heading">Encontre um profissional</h2>
 
-      <div className="search">
+      <fieldset className="search">
         <input
           id="search-input"
+          className="search__input"
           type="search"
           placeholder="Pesquise por projeto, skill ou função"
         />
-        <div id="search-icon">
-          <RiSearch2Line size={30} />
-        </div>
+        <label for="search-input" id="search-icon" className="search__label">
+          <a href="">
+            <RiSearch2Line size={30} />
+          </a>
+        </label>
+      </fieldset>
+      <div className="options">
+        <p className="options__name">Mais buscados:</p>
+        <ul className="options__list">
+          {list.map((item, index) => (
+            <li key={index} className="options__item">
+              {item.skill}
+            </li>
+          ))}
+        </ul>
       </div>
 
-      <img id="img2" src={img2} alt="" />
-    </div>
+      <img id="img2" src={img2} alt="decoração da pagina com ondas" />
+    </section>
   );
 };
