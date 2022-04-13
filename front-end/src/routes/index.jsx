f5c6e4452ed9568d4ef1192f8d92c18f8eb5f850
andresmartins910 <andresmartins910@gmail.com>
2022-04-13 18:37:27 -0300

    feat: header responsivity

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "../pages/HomePage";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 import { Dashboard } from "../pages/Dashboard";
 import { Profile } from "../pages/Profile";
+import { HomePage } from "../pages/HomePage";
+import App from "../App";
 
 export const AppRouting = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<App />} /> */}
         <Route path="signin" element={<SignIn />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="homePage" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
